4c73b473bceab30a64e602e3baac69eac92e7cb6
Camille Brett <camille.bretteville@gmail.com>
2019-12-18 12:59:23 +0100

    Inint detection; call cascades

---
+++ detection.py
@@ -0,0 +1,7 @@
+import cv2
+
+#Define cascade
+cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cascade_eye = cv2.CascadeClassifier('haarcascade_eye.xml')
+cascade_smile = cv2.CascadeClassifier('haarcascade_smile.xml')
+
